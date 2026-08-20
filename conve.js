94441fb43d903012f4030a246ec22cc6e91acd35
807080747 <38670428+807080747@users.noreply.github.com>
2026-08-20 15:34:47 +0800

    Update M3U source URL in conve.js

--- conve.js
+++ conve.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 // 你的M3U源地址，改成你自己的
-const M3U_SOURCE = "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u";
+const M3U_SOURCE = "https://seep.eu.org/https://live.445569.xyz/live.m3u";
 
 async function run() {
   try {
