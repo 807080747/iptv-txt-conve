404a7fbcbb99d96613510269d2aebb33310f376b
807080747 <38670428+807080747@users.noreply.github.com>
2026-09-09 09:10:40 +0800

    Remove a source URL from SOURCE_LIST

--- conve.js
+++ conve.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 // =========在这里添加所有源，txt、m3u都支持=========
 const SOURCE_LIST = [
-  "https://wget.la/https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",
+  
   "https://a.zbbs.eu.org/https://live.445569.xyz/live.m3u",
   "https://a.zbbs.eu.org/https://iptv.852851.xyz/sub/ebMaVCuF6lTu/playlist.m3u",
   "https://gh-proxy.org/https://raw.githubusercontent.com/pan8664716/MultiLive/main/output/douyin_live.m3u"
