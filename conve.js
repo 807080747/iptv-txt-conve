f0178f4f2de81010ffe76287ea0fd613f99996dd
807080747 <38670428+807080747@users.noreply.github.com>
2026-09-14 06:12:55 +0800

    Update SOURCE_LIST with new M3U links

--- conve.js
+++ conve.js
@@ -2,7 +2,8 @@ const fetch = require('node-fetch');
 
 // =========在这里添加所有源，txt、m3u都支持=========
 const SOURCE_LIST = [
-  "https://a.zbbs.eu.org/https://iptv.445569.xyz/live.m3u",
+  "http://ke.html-5.me/i/520.m3u",
+  "https://iptv.445569.xyz/live.m3u"
   "https://gh-proxy.org/https://iptv.852851.xyz/sub/ebMaVCuF6lTu/playlist.m3u"
 ];
 
