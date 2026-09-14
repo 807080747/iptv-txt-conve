ef3f08f3de4230883ca5b5071d6cb1a40d601ac8
807080747 <38670428+807080747@users.noreply.github.com>
2026-09-14 08:56:02 +0800

    Update SOURCE_LIST with new m3u links

--- conve.js
+++ conve.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 // =========在这里添加所有源，txt、m3u都支持=========
 const SOURCE_LIST = [
-"https://iptv.yang-1989.xyz/playlist.m3u","https://a.zbbs.eu.org/https://iptv.445569.xyz/live.m3u",
+"https://php.946985.filegear-sg.me/jackTV.m3u","https://iptv.445569.xyz/live.m3u",
   
 ];
 
