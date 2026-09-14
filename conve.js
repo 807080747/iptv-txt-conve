694cc7134697ad9d794feb39e49304b04863ea44
807080747 <38670428+807080747@users.noreply.github.com>
2026-09-14 08:39:22 +0800

    Fix formatting issues in conve.js

--- conve.js
+++ conve.js
@@ -2,8 +2,10 @@ const fetch = require('node-fetch');
 
 // =========在这里添加所有源，txt、m3u都支持=========
 const SOURCE_LIST = [
-"https://a.zbbs.eu.org/https://iptv.445569.xyz/live.m3u",
+"https://a.zbbs.eu.org/https://iptv.445569.xyz/live.m3u","https://wget.la/https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",
+  
 ];
+
 // 广告关键词
 const adKeywords = ["广告", "购物", "付费", "商城", "游戏推广", "财经广告", "弹窗", "TG频道"];
 
